7dfcb6fd27cce9cbdd315fd2f5cc6814c1f2f340
Mateus Ferreira <44096279+mfladeira@users.noreply.github.com>
2020-06-18 08:59:03 -0300

    updated

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,15 +3,15 @@ const path = require('path')
 const resolvers = require('./resolvers')
 const mongoose = require('mongoose')
 
-mongoose.connect("mongodb://localhost:27017/mongoQuestio", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false
-})
+// mongoose.connect("mongodb://localhost:27017/mongoQuestio", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useFindAndModify: false
+// })
 
-// mongoose.connect('mongodb://192.168.99.100:27017/mongoQuestio',function(){
-//     mongoose.connection.db.dropDatabase();
-// });
+mongoose.connect('mongodb://192.168.99.100:27017/mongoQuestio',function(){
+    mongoose.connection.db.dropDatabase();
+});
 
 const server = new GraphQLServer({
     typeDefs: path.resolve(__dirname, 'schema.graphql'),
